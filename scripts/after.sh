a2100d6175dc71a34dbd20302008ed343672fbe6
Rakesh-Echidna <rakesh.r@echidnainc.com>
2016-09-01 18:09:29 +0530

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,7 +1,5 @@
-source /home/ec2-user/.bash_profile
-cd /opt/alfresco-community/tomcat/webapps/Bachmans
-npm -g install karma bower
-npm -g install "gulpjs/gulp-cli#4.0"
-npm install
-bower install
-
+#source /home/ec2-user/.bash_profile
+#cd /opt/alfresco-community/tomcat/webapps/Bachmans
+#npm install --quiet -g grunt-cli karma bower
+#npm install
+#bower install
